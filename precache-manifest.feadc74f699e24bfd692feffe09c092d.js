43bb090ff00435754adafed566105bbcbca439e9
vinkabuki <vincent.kabacky@gmail.com>
2020-08-28 12:14:45 +0200

    Updates

---
+++ precache-manifest.feadc74f699e24bfd692feffe09c092d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbf8643473310a9cb046fc307490360",
+    "url": "/ti/index.html"
+  },
+  {
+    "revision": "7d5956a99afb38cb0180",
+    "url": "/ti/static/css/main.2d9f764c.chunk.css"
+  },
+  {
+    "revision": "791e811e5d4687ce42c0",
+    "url": "/ti/static/js/2.7e18dae2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ti/static/js/2.7e18dae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5956a99afb38cb0180",
+    "url": "/ti/static/js/main.9913c34d.chunk.js"
+  },
+  {
+    "revision": "c4f7be6f6ed14c92fa7c",
+    "url": "/ti/static/js/runtime-main.e0ac2ecf.js"
+  }
+]);
